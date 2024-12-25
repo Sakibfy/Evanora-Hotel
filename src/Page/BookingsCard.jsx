1b1d2bc4b087c91c7fe314a984dde964e7d9b4df
Md Sakib <gameingsakib71@gmail.com>
2024-12-25 18:32:00 +0600

    Deleted btn added

--- src/Page/BookingsCard.jsx
+++ src/Page/BookingsCard.jsx
@@ -1,11 +1,49 @@
+import Swal from "sweetalert2";
 
 
 const BookingsCard = ({ data,setRooms,rooms }) => {
-  console.log(data);
-const {name, image,description,price,startDate} = data
+  // console.log(data);
+const {name, image,description,price,startDate,_id} = data
 
+  const handleDelete = _id => {
+     console.log(_id);
+    Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    
+    fetch(`http://localhost:3000/bookingroom/${_id}`,{
+    method: "DELETE"
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+      Swal.fire({
+      title: "Deleted!",
+      text: "Your Booked Room has been deleted.",
+      icon: "success"
+          })
+          const remainingVisa = rooms.filter(visa => visa._id !== _id)
+          setRooms(remainingVisa);  
+          
+        }
+    })
+  }
+});
+
+    
+ }
+   
+     
   return (
-    <div className="border-2 p-2 rounded-lg">
+    <div className="border-2 p-2 rounded-lg duration-300 hover:scale-105">
       <img
             src={image}
               alt={name}
@@ -15,6 +53,9 @@ const {name, image,description,price,startDate} = data
         <p className="font-semibold text-[20px]">Price: ${price}</p>
       <p>Description: {description}</p>
       <p>Description: {startDate}</p>
+      <div className='text-right flex justify-end'>
+       <button onClick={()=> handleDelete (_id)} className=" py-2 px-3 border border-red-700 rounded-lg hover:bg-slate-300 duration-300 text-red-700 font-semibold ">Cancel</button>
+                </div>
     </div>    
       
   );
--- src/Page/MyBookings.jsx
+++ src/Page/MyBookings.jsx
@@ -10,8 +10,8 @@ const MyBookings = () => {
   
   return (
     <div>
-      <h2 className="text-4xl pb-5 font-bold text-[#4ca98d] duration-500 hover:text-[#438c76]  text-center mb-6  items-center ">My Booked Room</h2>
-      <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 w-11/12 mx-auto gap-8'>
+      <h2 className="text-4xl pb-5 font-bold text-[#4ca98d] duration-500 hover:text-[#438c76]  text-center mb-6  items-center ">My Booked Room </h2>
+      <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 w-11/12 mx-auto gap-8 mb-4'>
       {
           rooms.map(data => <BookingsCard
           key={data._id}
